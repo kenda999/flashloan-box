84e7172e2992c89288dac0d73a503dea1b8d7ed5
kenda999 <77441127+kenda999@users.noreply.github.com>
2021-02-12 13:52:20 +0300

    Update Withdrawable.sol

--- contracts/utils/Withdrawable.sol
+++ contracts/utils/Withdrawable.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.6.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
